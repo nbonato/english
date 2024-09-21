d37e22dd9d512b41cbb911e6ba004d1736a3691e
nbonato <akatux@gmail.com>
2024-09-21 23:25:41 +0200

    Add basic logic for new sentence and check buttons

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let sentenceParagraph = document.querySelector('#sentence-paragraph')
-
+let definitionParagraph = document.querySelector('#definition')
 
 
 
@@ -23,32 +23,46 @@ fetch('phrases.json')
 
 
 function getRandomLetter() {
-    let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    alphabet = 'BCDE';
-    const randomIndex = Math.floor(Math.random() * alphabet.length); // Get a random index
-    return alphabet[randomIndex]; // Return the letter at the random index
+  let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  alphabet = 'BCDE';
+  const randomIndex = Math.floor(Math.random() * alphabet.length); // Get a random index
+  return alphabet[randomIndex]; // Return the letter at the random index
 }
 
-
+let selectedLetter;
 function getRandomExcercise(sentences) {
-    let selectedLetter = getRandomLetter();
-    let subsetSentences = sentences[selectedLetter];
-    let randomExcercise = subsetSentences[Math.floor(Math.random() * subsetSentences.length)];
-    return randomExcercise
+  selectedLetter = getRandomLetter();
+  let subsetSentences = sentences[selectedLetter];
+  let randomExcercise = subsetSentences[Math.floor(Math.random() * subsetSentences.length)];
+  return randomExcercise
 }
+
+let wordSolution;
 
 function newSentence() {
-    let randomExercise = getRandomExcercise(sentences)
-    let exerciseSentence = randomExercise['phrase']
-    let inputBoxHTML = '<input type="text" placeholder="blank">'
-    
-    // Replacing text inside square brackets with input box
-    let currentSentence = exerciseSentence.replace(/\[.*?\]/, inputBoxHTML);
-    
-    // Find the word solution inside the text (between square brackets)
-    let match = exerciseSentence.match(/\[(.*?)\]/);
-    let wordToUse = match ? match[1] : null; // Extract the first captured group or null if not found
-    
-    sentenceParagraph.innerHTML = currentSentence
+  let randomExercise = getRandomExcercise(sentences)
+  let exerciseSentence = randomExercise['phrase']
+  let inputBoxHTML = '<input id="input-box" type="text" placeholder="..." style="text-align: center">'
+  
+  // Replacing text inside square brackets with input box
+  let currentSentence = exerciseSentence.replace(/\[.*?\]/, inputBoxHTML);
+  // Find the word solution inside the text (between square brackets)
+  let match = exerciseSentence.match(/\[(.*?)\]/);
+  wordSolution = match ? match[1] : null; // Extract the first captured group or null if not found
+  
+  sentenceParagraph.innerHTML = currentSentence
+  definitionParagraph.textContent = `${randomExercise['description']['simplified']} Starts with ${selectedLetter}`
 }
+
+
+function checkExercise() {
+  let attempt = document.querySelector("#input-box").value
+  if (attempt.trim().toLowerCase() === wordSolution.trim().toLowerCase()) {
+    alert("Great!")
+    newSentence()
+  } else {
+    alert("Wrong, try again!")
+  }
+}
+
 
