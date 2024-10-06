4440da7e8bd0c46d8f8be92c8aecbb478480423e
nbonato <akatux@gmail.com>
2024-10-06 20:32:16 +0200

    Store statistics in localStorage, change disabled styling

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ fetch('phrases.json')
     sentences = data; // Store the JSON data in the sentences variable
     // Start with the first sentence
     newSentence()
+
     skippedExercisesCounter.textContent = 0
   })
   .catch(error => {
@@ -51,7 +52,8 @@ let wordSolution;
 let inputBox;
 
 newSentenceButton.addEventListener('click', () => {
-  increaseCounter(skippedExercisesCounter);
+  statistics.skipped ++;
+  updateStatisticsDisplay(statistics)
 });
 
 function newSentence() {
@@ -78,6 +80,20 @@ function newSentence() {
     checkExerciseButton.disabled = inputBox.value.trim() === '';
     
   });
+
+  // Add an event listener to the input to check the exercise on
+  // pressing the Enter key.
+  // Add an event listener for the keydown event
+  inputBox.addEventListener('keydown', function(event) {
+    // Check if the key pressed is "Enter" and that the input
+    // box is not empty
+    if (event.key === 'Enter' && inputBox.value.trim() !== "") {
+      // Call the checkExercise function
+      checkExercise();
+    }
+  });
+
+
   inputBox.focus()
 }
 
@@ -93,63 +109,88 @@ let attempts = localStorage.getItem('attempts');
 if (attempts === null) {
   attempts = {};
 } else {
-  // If it exists, parse it (assuming it's a JSON object)
+  // If it exists, parse it
   attempts = JSON.parse(attempts);
 }
 
 
+// Check if attempts exists in localStorage
+let statistics = localStorage.getItem('statistics');
+
+// If it doesn't exist, initialise it as an empty object
+if (statistics === null) {
+  statistics = {
+    wrong: 0,
+    right: 0,
+    skipped: 0  
+  };
+} else {
+  // If it exists, parse it and populate the 
+  // statistics display
+  statistics = JSON.parse(statistics);
+  updateStatisticsDisplay(statistics)
+}
+
 
 
 function checkExercise() {
-  let userAnswer = document.querySelector("#input-box").value
+  let userAnswer = document.querySelector("#input-box").value.trim().toLowerCase()
   wordSolution = wordSolution.trim().toLowerCase()
 
 
-  let result = userAnswer.trim().toLowerCase() === wordSolution
-  updateAttempts(wordSolution, result)
+  let result = userAnswer === wordSolution
+  updateAttempts(wordSolution, result, userAnswer)
 
   if (result) {
     alert("Great!")
     newSentence()
-    increaseCounter(rightAnswersCounter)
+    statistics.right ++;
   } else {
     alert("Wrong, try again!")
-    increaseCounter(wrongAnswersCounter)
+    statistics.wrong ++;
     newSentenceButton.disabled = false
     showAnswerButton.style.visibility = 'visible'
     inputBox.value = ''
     inputBox.focus()
-  }  
+  }
+
+  updateStatisticsDisplay(statistics)
+
+  // Needs to be disabled otherwise you could check multiple times
+  // with an empty string
+  checkExerciseButton.disabled = true
+
+  // Store the statistics in localStorage
+  localStorage.setItem('statistics', JSON.stringify(statistics));   
 }
 
 
-function increaseCounter(counter) {
-  counter.textContent = parseInt(counter.textContent) + 1
-}
+
 
 
 
 /**
  * Updates the localStorage with the last time an 
  * exercise was attempted, indicating 
- * if successful or not.
+ * if successful or not and storing the attempt.
  *
  * @param {string} wordSolution - The solution to the exercise.
  * @param {boolean} result - Indicates whether the exercise was successful or not.
+ * @param {string} userAnswer - The user's attempt.
  * @returns {Object} The updated object for the current attempt
  *                   indicating the date and result of it.
  */
-function updateAttempts(wordSolution, result) {
+function updateAttempts(wordSolution, result, userAnswer) {
   // Get the current dateTime
   let dateTime = new Date().toISOString(); // Example value
   // Check if there's already a record for the current 
   // exercise in the attempts object
   if (wordSolution in attempts) {
     // If it exists, push the new dateTime and result to the existing object
-    attempts[wordSolution].push({ date: dateTime, success: result });
+    attempts[wordSolution].push({ date: dateTime, success: result , userAttempt: userAnswer});
   } else {
     // If it doesn't exist, create a new object
-    attempts[wordSolution] = [{ date: dateTime, success: result }];
+    attempts[wordSolution] = [{ date: dateTime, success: result, userAttempt: userAnswer }];
   }
 
   // Update the 'attempts' object in localStorage.
@@ -157,3 +198,15 @@ function updateAttempts(wordSolution, result) {
 }
 
 
+/**
+ * Update the statistics display with the content from 
+ * the "statistics" object.
+ * 
+ * @param {object} statistics - The object contains wrong, right and
+ *                              skipped counters.
+ */
+function updateStatisticsDisplay(statistics) {
+  rightAnswersCounter.textContent = parseInt(statistics.right);
+  wrongAnswersCounter.textContent = parseInt(statistics.wrong);
+  skippedExercisesCounter.textContent = parseInt(statistics.skipped);  
+}
